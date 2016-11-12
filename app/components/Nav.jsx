39b98c6cb4a5c7b3ec507be6d9792eb07e2e05fc
Tim Barland <Tim Barland>
2016-11-12 12:47:18 -0500

    Added timer and countdown components

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -10,14 +10,10 @@ var Nav = () => {
             React Timer App
           </li>
           <li>
-            <IndexLink to="/" activeClassName="active-link">
-              Timer
-            </IndexLink>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
           </li>
           <li>
-            <Link to="/" activeClassName="active-link">
-              Countdown
-            </Link>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
           </li>
         </ul>
       </div>
